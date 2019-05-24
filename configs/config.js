bb01e48c847e25c4a44d3a216b9bdc33aa90d8f7
qwertyuiop6 <super771783057@foxmail.com>
2019-05-24 20:38:01 +0800

    add config

---
+++ configs/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  appID: '',
+  appSecret: '',
+  dstname: '哲学♂世界',
+  adminlist: ['Butterfly', '',''],
+  rootlist: ['Butterfly'],
+  rootkey: ['del', 'reset'],
+
+  isSingleServer:true,
+  server: {
+    master: {
+      user: '',
+      ip: '',
+      port: '22',
+      passwd: '',
+    },
+    cave: {
+      user: '',
+      ip: '',
+      port: '22',
+      passwd: '',
+    },
+  },
+};
