51ebef81acc30523ded5a0ce3a063e94e03c1eb9
Butter fly <super771783057@foxmail.com>
2021-05-04 22:56:44 +0800

    add version

---
+++ service/version.js
@@ -0,0 +1,19 @@
+const cheerio = require('cheerio');
+const axios = require('axios');
+
+const getNew = async () => {
+    const res = await axios.get('https://forums.kleientertainment.com/game-updates/dst');
+    const $ = cheerio.load(res.data);
+    const vList = $('.ipsType_break');
+    const releaseV = vList.filter((i, el) => !$(el).find('span').text().includes('Test'));
+    releaseV.find('span').remove();
+    const newV=releaseV.text().split(' ').sort((a,b)=>b-a)[0].trim();
+    return newV;
+}
+
+const getLocal= () => fs.readFileSync(`/${process.env.HOME}/dst/version.txt`).toString().trim();
+
+module.exports={
+	getNew,
+	getLocal
+}
